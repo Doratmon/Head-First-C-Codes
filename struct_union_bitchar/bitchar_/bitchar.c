8f0c252b27eb740255c154c31095a547b63aceb7
Ethan-Windows <beyondself@foxmail.com>
2019-10-15 19:34:37 +0800

    编写了位字段的代码

--- struct_union_bitchar/bitchar_/bitchar.c
+++ struct_union_bitchar/bitchar_/bitchar.c
@@ -3,24 +3,26 @@
 //
 #include <stdio.h>
 
-typedef struct island {
-    char *name;
-    char *opens;
-    char *closes;
-    struct island *next;
-} island;
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*
+ * 水族馆问卷调查
+ */
+typedef struct {
+    unsigned int first_visit:1;//是否第一次参观
+    unsigned int come_again:1;//还会再来吗
+    unsigned int fingers_lost:4;//被食人鱼咬掉了几根手指
+    unsigned int shark_attack:1;//鲨鱼是否攻击人
+    unsigned int days_a_week:3;//一周来几次
+} survey;
 
 int main()
 {
-    island amity = {"Amity", "09:00", "17:00", NULL};
-    island craggy = {"Craggy", "09:00", "17:00", NULL};
-    island isla_nublar = {"Isla Nublar", "09:00", "17:00", NULL};
-    island shutter = {"Shutter", "09:00", "17:00", NULL};
-    amity.next = &craggy;
-    craggy.next = &isla_nublar;
-    isla_nublar = &shutter;
-    island skull = {"Skull", "09:00", "17:00", NULL};
-    isla_nublar.next = &skull;
-    skull.next = &shutter;
+    survey s;
+    printf("%d", sizeof(survey));
+    //printf("hello world");
+
     return 0;
 }
