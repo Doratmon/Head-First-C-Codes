bc5d83f5e124b83b8f590dee337caf385c991520
Ethan-Windows <beyondself@foxmail.com>
2019-10-15 19:35:45 +0800

    添加了注释

--- struct_union_bitchar/union_/bartender.c
+++ struct_union_bitchar/union_/bartender.c
@@ -3,7 +3,7 @@
 //
 #include <stdio.h>
 
-typedef union {
+typedef union {//定义一个联合 lemon变量与limit_pieces变量共用4个字节存储空间
     float lemon;
     int limit_pieces;
 } lemon_lime;
@@ -17,6 +17,7 @@ typedef struct {
 int main()
 {
     margarita m = {2.0, 1.0, {.limit_pieces=1}};
+//    margarita m = {2.0, 1.0, .citrus.limit_pieces=1}; //指定初始化器
     printf("%2.1f measures of tequila\n%2.1f measures of cointreau\n%i pieces of lime\n",
             m.tequila, m.cointreau, m.citrus.limit_pieces);
 
