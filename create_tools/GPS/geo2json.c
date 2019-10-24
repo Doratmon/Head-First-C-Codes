f57a24cbc6ed848190c610698642213af69dac74
Ethan-Windows <beyondself@foxmail.com>
2019-10-24 12:57:39 +0800

    调整了json输出格式，过滤无效经纬经纬度

--- create_tools/GPS/geo2json.c
+++ create_tools/GPS/geo2json.c
@@ -9,18 +9,26 @@
 int main()
 {
     float latitude;
-    float longtitude;
+    float longitude;
     char info[80];
     int started = 0;
 
-    puts("data=[");
-    while(scanf("%f, %f, %79[^\n]",&latitude ,&longtitude ,info) == 3) {
+    puts("{\"data\":[");
+    while(scanf("%f, %f, %79[^\n]",&latitude ,&longitude ,info) == 3) {//scanf()返回成功读取数据条数
         if (started)
-            printf("\n");
+            printf(",\n");
         else
             started = 1;
-        printf("{latitude: %f, longtitude: %f, info: '%s'}",latitude ,longtitude ,info );
+        if((latitude < -90.0) || (latitude > 90.0)) { //如果输入的纬度无效，将输出发送到标准错误
+            fprintf(stderr, "Invalid latitude %f\n", latitude);
+            return 2;
+        }
+        if((longitude < -180.0) || (longitude > 180.0)) { //如果输入的经度无效，将输出发送到标准错误
+            fprintf(stderr, "Invalid longitude %f\n", longitude);
+            return 2;
+        }
+        printf("{\"latitude\": %f, \"longitude\": %f, \"info\": \"%s\"}",latitude ,longitude ,info );
     }
-    puts("\n]");
+    puts("]\n}");
     return 0;
 }
