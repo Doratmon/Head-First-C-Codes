804f115b132604e69e9f4f5636c44e622260bb53
Ethan <lean.we@outlook.com>
2020-11-06 11:21:03 +0800

    add 'label' function

--- struct_union_bitchar/struct_/fish.c
+++ struct_union_bitchar/struct_/fish.c
@@ -25,14 +25,17 @@ struct fish {
     struct preferences care;
 };
 
+void label(struct fish snappy);
 int main()
 {
-    struct fish snappy = {"Snappy", "Piranha", 64, 4, {{"meat", 0.2}, {"swim in the jacuzzi", 7.5}}};
-    printf("Name:%s Species:%s Teeth:%i Age:%i \n",
-            snappy.name,snappy.species,snappy.teeth,snappy.age);
-    printf("Feed with %2.2f 1bs of %s and allow to %s for %2.2f hours\n",
-            snappy.care.food.weight, snappy.care.food.ingredients, snappy.care.exercises.description, snappy.care.exercises.duration);
-
+    struct fish snappy = {"snappy", "Pizaha",69,4,{{"meat",0.2},{"swim in the jacuzzi",7.5}}};
+    label(snappy);
     return 0;
 }
-
+void label(struct fish snappy)
+{
+    printf("Name:%s Species:%s Teeth:%d Age:%d\n",
+            snappy.name, snappy.species, snappy.teeth, snappy.age);
+    printf("Feed with %2.2f 1bs of %s and allow to %s for %2.2f hours\n",
+            snappy.care.food.weight, snappy.care.food.ingredients, snappy.care.exercises.description, snappy.care.exercises.duration);
+}
